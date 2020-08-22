4e1b42c6a7c5468e1c9ba498cfbc7bd99091e2a6
Bartek Ciszkowski <bart.ciszk@gmail.com>
2020-08-21 21:51:35 -0400

    Initial Commit

---
+++ swiftui-mapkit-location-sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-mapkit-location-sample
+//
+//  Created by Bartek Ciszkowski on 2020-08-21.
+//  Copyright © 2020 Bartek Ciszkowski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
