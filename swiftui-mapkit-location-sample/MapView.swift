baa9024e7ce0005a02ed73dd9d26f90e6b2c44f4
Bartek Ciszkowski <bart.ciszk@gmail.com>
2020-08-26 06:38:25 -0400

    Adds annotating and centering map on with searched items
    This commit adds the ability to click on search results and annotate the
    map with the selected result. Further annotations can be added and the
    map will center to the most recent annotation whenever it is added.
    
    If the users location change, the search region is adjusted (most of the
    time, see quirks).
    
    This continues to test my new-found understanding of architecture within
    iOS, as I deal with both a LocationManager (for user location) and a
    SearchService (for finding points of interest relative to region) in the
    context of a single map view.
    
    There are a few points where I grab data from these managers in
    ContentView to adjust state and I don't think this is the right
    approach. There's other update-y things happening in MapView when
    annotations are changed but this doesn't feel "right"
    
    Likely a blend of poor architecture and my current uncertainty in how to
    structure this in iOS.
    
    At the least, it works!
    
    A few quirks yet:
    
    * When the TextField appears for searching, it is not focused. This is
      driving me mad and is not possible in SwiftUI yet. Although it can be
      done with a blend of non-SwiftUI code and itself, I don't know if it's
      worth it for the sake of this sample
    * The region is respected, sometimes! It might be an Xcode Simulator
      issue but sometimes I see "Phoenox, Arizona" in searches when I'm in
      Tokyo.

--- swiftui-mapkit-location-sample/MapView.swift
+++ swiftui-mapkit-location-sample/MapView.swift
@@ -11,9 +11,11 @@ import MapKit
 
 struct MapView: UIViewRepresentable {
     
+    var centerCoordinate: CLLocationCoordinate2D
+    
+    var annotations: [MKPointAnnotation]
+    
     class Coordinator: NSObject, MKMapViewDelegate {
-        @EnvironmentObject var locationService: LocationService
-
         var parent: MapView
         
         init(_ parent: MapView) {
@@ -22,12 +24,28 @@ struct MapView: UIViewRepresentable {
         
         func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
         }
+        
+    
+        // Customize how annotations look and function
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let identifier = "Annotation"
+            
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            
+            if annotationView == nil {
+                annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                annotationView?.canShowCallout = false
+            } else {
+                annotationView?.annotation = annotation
+            }
+            
+            return annotationView
+        }
     }
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-    
     
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView(frame:  UIScreen.main.bounds)
@@ -36,17 +54,32 @@ struct MapView: UIViewRepresentable {
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
         
-
         return mapView
     }
     
     func updateUIView(_ view: MKMapView, context: Context) {
+        // Only want a single annotation on the map for this particular demo
+        view.removeAnnotations(view.annotations)
+        if annotations.count > 0 {
+            view.addAnnotations(annotations)
+            
+            // Center to the new annotation
+            view.setCenter(centerCoordinate, animated: true)
+        }
     }
 }
 
+extension MKPointAnnotation {
+    static var example: MKPointAnnotation {
+        let annotation = MKPointAnnotation()
+        annotation.title = "Apple"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 35.702069, longitude: 139.775327)
+        return annotation
+    }
+}
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(centerCoordinate: MKPointAnnotation.example.coordinate, annotations: [MKPointAnnotation.example])
     }
 }
